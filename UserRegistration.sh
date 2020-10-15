4ffeb17ec18110db2c7b948a5113003956910776
Vikash Kanna <Enter Git Registered Email id>
2020-10-15 08:09:58 +0530

    Validate First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,1 +1,20 @@
-Echo "Welcome To User Registration"
+#!/usr/local/bin/bash -x
+
+echo "Welcome To User Registration"
+
+echo "Enter Your First Name"
+while read value1
+
+FirstName="^[A-Z][A-Za-z]{1,3}$"
+
+do
+
+if [[ $value1 =~ $FirstName ]]
+then
+        echo true;
+        break
+else
+        echo "False Input,Enter Your Name Again"
+        fi
+                done
+
